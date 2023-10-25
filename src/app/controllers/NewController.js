872c3e9fd0acc7f420fb9921ac09ac60329fa040
Lê Ngọc Phúc <phuc97405@gmail.com>
2023-10-25 11:28:29 +0700

    testPre

--- src/app/controllers/NewController.js
+++ src/app/controllers/NewController.js
@@ -1,11 +1,11 @@
 class NewController {
-    index(req, res) {
-        res.render('news');
-    }
-    //[GET] /news/slug
-    show(req, res) {
-        res.send('NEW DETAILT');
-    }
+  index(req, res) {
+    res.render("news");
+  }
+  //[GET] /news/slug
+  show(req, res) {
+    res.send("NEW DETAILT");
+  }
 }
 
 module.exports = new NewController();
