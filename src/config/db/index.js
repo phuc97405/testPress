2433e5ec8fe464e0dd31a96df46fab89ee066c01
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-06 00:25:16 +0700

    deploying

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose
-      .connect("mongodb://localhost:27017/press_dev")
+      .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/press_dev")
+      // connect("mongodb://localhost:27017/press_dev")
       .then(() => console.log("Connected!"));
   } catch (error) {
     console.log("Error connecting");
