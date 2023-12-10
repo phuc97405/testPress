6ef1e9f8d8e576d6f6f4b81a7e0edb9635e5f09e
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-10 13:55:22 +0700

    adssada

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { options, use } = require("../../routes/news");
+const { userInfo } = require("os");
 require("dotenv").config();
 
 
@@ -6,7 +8,11 @@ async function connect() {
   try {
     // console.log(`MONGODB_URI:, ${pro}`);
     await mongoose.connect(
-      'mongodb+srv://phuc97405:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' ,
+      'mongodb://atlas-sql-651e77b4f570e15ce79954b8-r8yuh.a.query.mongodb.net/press_dev?ssl=true&authSource=admin',
+      
+      {user:'phuc2023',pass:'phuc2023'}
+    
+      // 'mongodb+srv://phuc97405:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' ,
       // 'mongodb://atlas-sql-651e77b4f570e15ce79954b8-r8yuh.a.query.mongodb.net/press_dev?ssl=true&authSource=admin'
     );
     // .connect("mongodb://localhost:27017/press_dev")
