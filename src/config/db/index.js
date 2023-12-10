e297fa14de8fe96e57b1e20c8c85c0270e4a3f33
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-10 12:35:13 +0700

    asdas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -6,7 +6,8 @@ async function connect() {
   try {
     // console.log(`MONGODB_URI:, ${pro}`);
     await mongoose.connect(
-      'mongodb+srv://phuc2023:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' 
+      // 'mongodb+srv://phuc2023:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' ,
+      'mongodb://atlas-sql-651e77b4f570e15ce79954b8-r8yuh.a.query.mongodb.net/press_dev?ssl=true&authSource=admin'
     );
     // .connect("mongodb://localhost:27017/press_dev")
     console.log("Connected Mongo!");
