f59d4b61967f6fff815ce2854e7a74b936d882a7
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-10 11:46:08 +0700

    aa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const methodOverride = require("method-override");
 
 const { engine } = require("express-handlebars");
