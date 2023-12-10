e23108f310e53669b293caaeac0c0d7d174e6361
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-10 12:02:13 +0700

    asdasd

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 
 async function connect() {
   try {
+    console.log(`MONGODB_URI:, ${process.env.MONGODB_URI}`);
     await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/press_dev"
+      'mongodb+srv://phuc2023:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' 
     );
     // .connect("mongodb://localhost:27017/press_dev")
     console.log("Connected!");
