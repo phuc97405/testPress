658965bb38ff4fccd694cc03428027882bf6029f
BM Pista <74225031+phuc97405@users.noreply.github.com>
2023-12-10 12:26:29 +0700

    sdasd

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 
 async function connect() {
   try {
-    console.log(`MONGODB_URI:, ${process.env.MONGODB_URI}`);
+    // console.log(`MONGODB_URI:, ${pro}`);
     await mongoose.connect(
       'mongodb+srv://phuc2023:phuc2023@cluster0.adg3mlb.mongodb.net/?retryWrites=true&w=majority' 
     );
     // .connect("mongodb://localhost:27017/press_dev")
-    console.log("Connected!");
+    console.log("Connected Mongo!");
   } catch (error) {
-    console.log("Error connecting");
+    console.log(`Error connecting ${error}`);
   }
 }
 module.exports = { connect };
